9b33f61cf325aee22a5c25f7184397ee16f5d990
LJ D. <87740926+cold-carmel@users.noreply.github.com>
2024-10-29 19:52:28 -0400

    Added multiplayer functionality

--- BattleshipMinigame/laser.gd
+++ BattleshipMinigame/laser.gd
@@ -3,6 +3,7 @@ extends Area3D
 const LASER_SPEED = 50.0 # Speed of the laser
 
 var direction : Vector3 = Vector3()
+var shooter : CharacterBody3D
 
 func _physics_process(delta: float) -> void:
 	# Move the laser forward
@@ -11,7 +12,23 @@ func _physics_process(delta: float) -> void:
 	# Optional: Remove the laser after it travels far enough or collides
 	if global_transform.origin.length() > 1000: # or some distance threshold
 		queue_free()
+	
 
 # Set the laser's direction
 func set_direction(dir: Vector3) -> void:
 	direction = dir.normalized()
+	
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is StaticBody3D:
+		queue_free()
+	
+	if body == shooter:
+		return
+	
+	if body is BSMPlayer:
+		body.lives_remaining -= 1
+		print(body.name + " was shot by " + shooter.name)
+		print(body.name + " has " + str(body.lives_remaining) + " lives remaining")
+		print()
--- BattleshipMinigame/player_1.gd
+++ BattleshipMinigame/player_1.gd
@@ -1,10 +1,11 @@
+class_name BSMPlayer
 extends CharacterBody3D
 
 
 const MAX_LIN_SPEED = 20.0 # m/sec
 const LIN_ACCEL = 40.0 # m/sec^2
 const MAX_ROTATIONAL_VELO = 5.0 # rads/sec
-const ROTATIONAL_ACCEL = 20.0 # rads/sec^2
+const ROTATIONAL_ACCEL = 30.0 # rads/sec^2
 const TARGET_CENTRIP_ACCEL = 75.0 # m/sec^2
 const CROSS_PROD_DEADZONE = 0.0001 # unitless
 const INPUT_VECTOR_DEADZONE = 0.2 # unitless
@@ -14,24 +15,25 @@ var target_lin_velo : float = 0.0 # m/sec
 var lin_velo : float = 0.0 # m/sec
 var target_rotational_velo : float = 0.0 # rads/sec
 var rotational_velo : float = 0.0 # rads/sec
-var cycle_num : int = 0
-var time_since_last_laser : float = 0.0
+var cycle_num : int = 0 # used for debug printing
+var time_since_last_laser : float = 0.0 # cooldown for shooting laser
+var lives_remaining : int = 3
 
 
 @export var player: Controls.Player
 @export var color: Color
 @export var laser_scene : PackedScene
 
-
+func _ready() -> void:
+	var tip_material = $Tip.get_surface_override_material(0)
+	if tip_material is StandardMaterial3D:
+		tip_material.albedo_color = color
 
 func _physics_process(delta: float) -> void:
 	# ! Physics Processing
 	
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
 	var input_dir : Vector2 = Controls.get_vector(player, "core_player_left", "core_player_right", "core_player_up", "core_player_down")
-	var forward_dir = Vector2(-transform.basis.z.normalized().x, -transform.basis.z.normalized().z)
+	var forward_dir : Vector2 = Vector2(-transform.basis.z.normalized().x, -transform.basis.z.normalized().z)
 	set_target_rotational_velo(input_dir, forward_dir)
 	set_target_lin_velo(input_dir)
 	
@@ -53,8 +55,11 @@ func _physics_process(delta: float) -> void:
 	velocity.x = forward_dir.x * lin_velo
 	velocity.z = forward_dir.y * lin_velo # forward_dir is a Vector2, so grab the y
 
+	print(str(position.y))
+
 	cycle_num += 1
 	move_and_slide()
+	
 	
 	# ! Laser Spawning
 	time_since_last_laser += delta
@@ -62,7 +67,11 @@ func _physics_process(delta: float) -> void:
 	if (Controls.is_action_just_pressed(player, "core_player_jump")) and (time_since_last_laser > LASER_COOLDOWN_TIME):
 		shoot_laser()
 		time_since_last_laser = 0
-		
+	
+	
+	# ! Lives Handling
+	if (lives_remaining == 0):
+		queue_free()
 
 func set_target_rotational_velo(input_vector: Vector2, forward_vector: Vector2):
 	var cross_prod : float = input_vector.cross(forward_vector)
@@ -89,6 +98,7 @@ func shoot_laser():
 	# Set the laser's position at the marker position
 	laser.global_transform.origin = laser_spawn_point.global_transform.origin
 	laser.rotation = self.rotation
+	laser.shooter = self
 
 	# Set the laser's direction (same as the forward direction of the spaceship)
 	laser.set_direction(-transform.basis.z.normalized())
