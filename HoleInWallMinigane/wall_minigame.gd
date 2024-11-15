b918c8b532b71b183bbefae1f401413b315f18a9
jake0822 <user@FIH-COM23a108.fsu.edu>
2024-11-14 19:24:06 -0500

    Adds Spawner and Adds score

--- HoleInWallMinigane/wall_minigame.gd
+++ HoleInWallMinigane/wall_minigame.gd
@@ -2,11 +2,14 @@ extends Node3D
 @onready var _p1 := $Wall_Player3
 @onready var _p2 := $Wall_Player4
 @onready var _line: Path3D = $Path3D
-
+@onready var scoreLabel: Label = $HBoxContainer/SubViewportContainer2/Score
+var score = 3
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	scoreLabel.text = str(score)
 	for i in range(_line.curve.point_count):
 		print(_line.curve.get_point_position(i))
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +25,5 @@ func _on_area_3d_body_entered(body):
 
 func _on_area_3d_area_entered(area):
 	print("Area collision occured!")
+	score = score - 1
+	scoreLabel.text = str(score) 
