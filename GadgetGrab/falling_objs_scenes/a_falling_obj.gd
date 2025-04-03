8ee35bc05bf79b3cb8e51401ebeaaa32593397d2
LJ D. <87740926+cold-carmel@users.noreply.github.com>
2025-04-03 18:23:54 -0400

    Added falling obj system
    The falling obj system uses resources, make one for each new falling obj

--- GadgetGrab/falling_objs_scenes/a_falling_obj.gd
+++ GadgetGrab/falling_objs_scenes/a_falling_obj.gd
@@ -3,20 +3,21 @@ extends Area3D
 
 @export var falling_obj_res: FallingObjRes
 
-const FALLING_SPEED : float = 0.2
-
+const FALLING_SPEED : float = -0.8
 func _ready():
 	add_child(falling_obj_res.objModel.instantiate())
 	$CollisionShape3D.shape.size = Vector3.ONE * falling_obj_res.collision_size
 
 func _physics_process(delta: float) -> void:
-	position.y += FALLING_SPEED
+	position.y += FALLING_SPEED * delta
 	
 func _on_body_entered(body: Node3D) -> void:
+	print("Ping!")
 	if body is GrabPlayer:
 		body.objs_collected += 1
 		queue_free()
 	elif body is not AFallingObj:
+		await get_tree().create_timer(2).timeout
 		queue_free()
 	
 	
--- GadgetGrab/main.gd
+++ GadgetGrab/main.gd
@@ -1,9 +1,32 @@
 extends Node3D
 
-@export var falling_orb_scene = preload("res://GadgetGrab/falling_objs/falling_orb.tscn")
 @export var a_falling_obj = preload("res://GadgetGrab/falling_objs_scenes/a_falling_obj.tscn")
 
+func get_all_resources() -> Array[FallingObjRes]:
+
+	
+	var dir = DirAccess.open("res://GadgetGrab/GradgetGrabResources")
+	
+	if not dir:
+		return []
+	
+	var resources : Array[FallingObjRes] = []
+	
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if file_name.get_extension() == "tres":
+			var resource = ResourceLoader.load("res://GadgetGrab/GradgetGrabResources/" + file_name)
+			if resource is FallingObjRes:
+				resources.append(resource)
+		file_name = dir.get_next()
+	
+	dir.list_dir_end()
+	
+	return resources
+
 var rng = RandomNumberGenerator.new()
+var resources : Array[FallingObjRes] = get_all_resources()
 
 func _ready():
 	rng.randomize()
@@ -11,14 +34,15 @@ func _ready():
 
 func spawn_random_object(position: Vector3):
 	var falling_obj_instance = a_falling_obj.instantiate()
+	falling_obj_instance.falling_obj_res = resources.pick_random()
 	add_child(falling_obj_instance)
 	falling_obj_instance.position = position
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	var pos : Vector3
-	pos.x = randf_range(-230,230)
+	pos.x = randf_range(-20,20)
 	pos.y = 10
-	pos.z = randf_range(-230,230)
+	pos.z = randf_range(-20,20)
 	#var random = randf_range(0,800)
 	if (((pos.x*pos.x) +(pos.z*pos.z)) <= 225):
 		spawn_random_object(pos)
