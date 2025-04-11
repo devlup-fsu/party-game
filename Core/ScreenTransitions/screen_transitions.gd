3029c2fc135e2c07fa2dfd90646ee3ac4bcfabf0
7Limes <milesb2288@gmail.com>
2025-04-11 19:44:08 -0400

    Rename create_fade_scene

--- Core/ScreenTransitions/screen_transitions.gd
+++ Core/ScreenTransitions/screen_transitions.gd
@@ -6,7 +6,7 @@ extends Node
 
 ## Create a new fade scene.
 ## If `fade_out` is true, the current scene will fade out. Otherwise, the scene will fade in.
-func create_fade_scene(fade_out: bool, onexit: Callable) -> FadeScene:
+func _create_fade_scene(fade_out: bool, onexit: Callable) -> FadeScene:
 	var scene = fade_scene.instantiate()
 	scene.onexit = onexit
 	scene.fade_type = fade_out
@@ -19,12 +19,12 @@ func fade(next_scene: Node):
 		SceneManager._pop_scene().queue_free()
 	
 	var fade_out_exit = func():
-		var fade_in_scene = create_fade_scene(false, fade_in_exit)
+		var fade_in_scene = _create_fade_scene(false, fade_in_exit)
 		SceneManager._pop_scene().queue_free()  # pop the fade out scene
 		SceneManager._push_scene(next_scene)
 		SceneManager._push_scene(fade_in_scene, false)
 	
-	var fade_out_scene = create_fade_scene(true, fade_out_exit)
+	var fade_out_scene = _create_fade_scene(true, fade_out_exit)
 	SceneManager._push_scene(fade_out_scene, false)
 
 
@@ -34,9 +34,9 @@ func fade_to_board():
 		SceneManager._pop_scene().queue_free()
 	
 	var fade_out_exit = func():
-		var fade_in_scene = create_fade_scene(false, fade_in_exit)
+		var fade_in_scene = _create_fade_scene(false, fade_in_exit)
 		SceneManager._pop_to_board()
 		SceneManager._push_scene(fade_in_scene, false)
 	
-	var fade_out_scene = create_fade_scene(true, fade_out_exit)
+	var fade_out_scene = _create_fade_scene(true, fade_out_exit)
 	SceneManager._push_scene(fade_out_scene, false)
