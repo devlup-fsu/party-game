b0762102d7698c4bfcba2b501d8c8243ba625c88
7Limes <milesb2288@gmail.com>
2025-04-11 19:17:27 -0400

    Implement fade transition

---
+++ Core/ScreenTransitions/fade_scene.gd
@@ -0,0 +1,30 @@
+extends Node2D
+class_name FadeScene
+
+const FADE_TIME = 1.0
+
+## Whether to fade to black (true) or fade in from black (false).
+var fade_type: bool = true
+
+## The function to call when the fade is finished.
+var onexit: Callable
+
+var fade_timer: float = 0.0
+
+
+func _ready() -> void:
+	if fade_type == false:
+		$FadeRect.color.a = 1.0
+	
+
+func _process(delta: float) -> void:
+	fade_timer = move_toward(fade_timer, FADE_TIME, delta)
+	if fade_timer >= FADE_TIME:
+		self.onexit.call()
+	else:
+		var alpha: float
+		if fade_type:  # fade out
+			alpha = remap(fade_timer, 0.0, FADE_TIME, 0.0, 1.0)
+		else:
+			alpha = remap(fade_timer, 0.0, FADE_TIME, 1.0, 0.0)
+		$FadeRect.color.a = alpha
