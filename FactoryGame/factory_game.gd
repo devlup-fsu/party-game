4bbf9352fc68a5de07c4f99d56a6fe8d3a6571ff
7Limes <milesb2288@gmail.com>
2025-04-12 15:36:34 -0400

    Implement placements

--- FactoryGame/factory_game.gd
+++ FactoryGame/factory_game.gd
@@ -20,6 +20,27 @@ func _physics_process(delta: float) -> void:
 	%Score4.text = str(players_list[3].points)
 
 
+func get_player_rankings(scores: Array[float], lower_is_better: bool=false) -> Array[Scores.Place]:
+	var unique_scores: Array[float] = []
+	for score in scores:
+		if not score in unique_scores:
+			unique_scores.append(score)
+	
+	unique_scores.sort()
+	if lower_is_better:
+		unique_scores.reverse()
+	
+	var score_to_place: Dictionary[float, Scores.Place] = {}
+	for place in range(unique_scores.size()):
+		score_to_place[unique_scores[place]] = place
+	
+	var placements: Array[Scores.Place] = []
+	for score in scores:
+		placements.append(score_to_place[score])
+	
+	return placements
+
+
 func _on_timer_timeout() -> void:
 	if seconds_remaining > 0:
 		var minute = seconds_remaining / 60
@@ -28,8 +49,10 @@ func _on_timer_timeout() -> void:
 			%TimerLabel.text = "%d:0%d" % [minute, seconds]
 		else:
 			%TimerLabel.text = "%d:%d" % [minute, seconds]
-		
 		seconds_remaining -= 1
-	
 	else:
-		SceneManager.exit_minigame(0, 0, 0, 0)
+		var scores: Array[float] = []
+		for player: FactoryPlayer in players_list:
+			scores.append(float(player.points))
+		var placements = get_player_rankings(scores)
+		SceneManager.exit_minigame(placements[0], placements[1], placements[2], placements[3])
