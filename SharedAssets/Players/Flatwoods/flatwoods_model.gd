72885acf4087c33b0e207e989d537646d8406376
Dion Tryban <dion@tryban.dev>
2025-09-03 19:18:43 -0400

    fix: use correct shader for flatwoods player

--- SharedAssets/Players/Flatwoods/flatwoods_model.gd
+++ SharedAssets/Players/Flatwoods/flatwoods_model.gd
@@ -1,4 +1,8 @@
 @tool
 extends Node3D
 
+@onready var flatwoods_mesh: MeshInstance3D = $FlatwoodsGameReady/FlatArma/Skeleton3D/FlatWoods
 @onready var animation_tree: AnimationTree = $AnimationTree
+
+func _ready() -> void:
+	flatwoods_mesh.mesh.surface_set_material(0, load("res://SharedAssets/Players/Flatwoods/flatwoods_shader_mat.tres"))
